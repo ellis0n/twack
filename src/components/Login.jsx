31be34028bb4e0165b48a82334703266ccc5bc4c
glen may <ellisonglenmay@gmail.com>
2022-12-15 15:57:45 -0330

    working login/registratoin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,104 +1,97 @@
-import { React, useEffect, useState, useContext } from 'react'
-import AuthContext from '../context/AuthProvider';
-import Banner from './Banner'
-
+import { React, useEffect, useState } from "react";
+import Banner from "./Banner";
+import useAuth from "../hooks/useAuth";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const Login = () => {
+  const { setAuth } = useAuth();
 
-    const { setAuth } = useContext(AuthContext);
-    const [user, setUser] = useState("");
-    const [pwd, setPwd] = useState("");    
-    const [err, setErr] = useState("");
-    const [success, setSuccess] = useState(false);
+  const { navigate } = useNavigate();
+  const { location } = useLocation();
 
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [err, setErr] = useState("");
 
-    useEffect(() => {
-        setErr("");
-    }, [user, pwd])
+  useEffect(() => {
+    setErr("");
+  }, [user, pwd]);
 
-    
-    const handleClick = async(e) => {
-        e.preventDefault();
-        const data = JSON.stringify({ user, pwd });
-        await fetch("http://localhost:3500/login", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                withCredentials: true
-            },
-            body: data
-        })
-          .then((response) => {
-            if (response.status === 401){
-              throw new Error("Invalid username or password")
-            } else if (
-                response.status === 400){
-                throw new Error("Missing username or password")
-            }
-            console.log(response)
-            setSuccess(true);
-            setUser('');
-            setPwd('');
-            })
-          .catch((err) => {
-            setErr(err)
-            })
-    }
-    
-    return (
-    <>
-    <Banner className ="banner"/>
-    {success ? (
-        <>
-            <div className = "true">
-                <h1>Welcome back.</h1>
-            </div>
-            <div className = "logo">
-                <p>
-                    <a href="/ads">Get twacking.</a>
-                </p>
-            </div>
-        </>
-    ) : (
-
-    <div className="login">
-        {err ?
-        <div className = "error-msg">
-            <h4>{`${err}`}</h4>
-        </div> :
-        null
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const data = JSON.stringify({ user, pwd });
+    await fetch("http://localhost:3500/auth", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        withCredentials: true,
+      },
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        if (response.status === 401) {
+          throw new Error("Invalid username or password");
+        } else if (response.status === 400) {
+          throw new Error("Missing username or password");
         }
-        <div className = "login-form">
-            <form> 
-                <label htmlFor="username">
-                <div className = "input-value">
-                    Username:
-                </div>
-                <input 
-                    type="text"
-                    value={user} 
-                    autoComplete="off"
-                    onChange={(e) => setUser(e.target.value)}  className = "text-input"/>
-                </label>
+        const accessToken = response.accessToken;
+        const roles = response.roles;
+        console.log(accessToken, roles);
+        setAuth({ user, pwd, roles, accessToken });
+        setUser("");
+        setPwd("");
+      })
+      .catch((err) => {
+        setErr(err);
+      });
+  };
 
-                <label >
-                    <div className = "input-value">Password:</div>
-                    <input type="password" value={pwd} onChange={(e) => setPwd(e.target.value)} className = "text-input"/>
-                </label>
+  return (
+    <>
+      <Banner className="banner" />
+      <div className="login">
+        {err ? (
+          <div className="error-msg">
+            <h4>{`${err}`}</h4>
+          </div>
+        ) : null}
+        <div className="login-form">
+          <form>
+            <label htmlFor="username">
+              <div className="input-value">Username:</div>
+              <input
+                type="text"
+                value={user}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                className="text-input"
+              />
+            </label>
 
-                <button className = "login-btn" onClick={handleClick}>
-                    Sign In
-                </button>
-            </form>
+            <label>
+              <div className="input-value">Password:</div>
+              <input
+                type="password"
+                value={pwd}
+                onChange={(e) => setPwd(e.target.value)}
+                className="text-input"
+              />
+            </label>
+
+            <button className="login-btn" onClick={handleClick}>
+              Sign In
+            </button>
+          </form>
         </div>
-        <h4 className = "auth-link">
-            <a href="/login">New here?</a>
+        <h4 className="auth-link">
+          <Link to="/register">New here?</Link>
         </h4>
-    </div>
-    )}
+      </div>
     </>
-    )
-}
+  );
+};
 
-export default Login
+export default Login;
