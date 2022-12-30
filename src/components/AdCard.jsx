b649be166d2b32195a12db5db7f95f334bba0dc7
glen may <ellisonglenmay@gmail.com>
2022-12-29 20:56:47 -0330

    save pref to user document

--- src/components/AdCard.jsx
+++ src/components/AdCard.jsx
@@ -6,7 +6,7 @@ import ParamBox from "./ParamBox";
 import VoteButton from "./VoteButton";
 import Banner from "./Banner";
 import Navbar from "./Navbar";
-import useAuth from '../hooks/useAuth';
+import useAuth from "../hooks/useAuth";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import { useNavigate, useLocation } from "react-router-dom";
 
@@ -22,16 +22,15 @@ const AdCard = () => {
   const [votes, setVotes] = useState([]);
   const [running, setRunning] = useState(false);
 
-  
   useEffect(() => {
     const updateParams = async () => {
       await fetch("http://localhost:3500/pref", {
         method: "GET",
-        credentials: 'include',
+        credentials: "include",
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-          Authorization: `Bearer: ${ auth.accessToken }`
+          Authorization: `Bearer: ${auth.accessToken}`,
         },
       })
         .then((response) => response.json())
@@ -49,59 +48,54 @@ const AdCard = () => {
 
   const scrapeAds = async (params) => {
     try {
-        const response = await axiosPrivate.post('/scrape', JSON.stringify(params),
+      const response = await axiosPrivate.post(
+        "/scrape",
+        JSON.stringify(params),
         {
-            headers: { 'Content-Type': 'application/json' },
-            withCredentials: true,
-        });
-        setAds(JSON.parse(response.data)
-        );
-        setRunning(true);
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      setAds(JSON.parse(response.data));
+      setRunning(true);
     } catch (err) {
-        console.error(err);
-        navigate('/login', { state: { from: stateLocation }, replace: true });
+      console.error(err);
+      navigate("/login", { state: { from: stateLocation }, replace: true });
     }
-}
+  };
 
   const sendVote = async (vote) => {
-      try {
-          const response = await axiosPrivate.post('/vote', JSON.stringify(vote),
-          {
-              headers: { 'Content-Type': 'application/json' },
-              withCredentials: true,
-          });
-          console.log(`Voted ${response.data.vote} for ${response.data.ad.id}`)
-          setVotes([...votes, vote]);
-          setAds(ads.filter((ad) => ad.id !== vote.ad.id));
-          if (ads.length === 1) {
-            setVotes([]);
-            setRunning(false);
-          }
-      } catch (err) {
-          console.error(err);
-          navigate('/login', { state: { from: stateLocation }, replace: true });
+    try {
+      const response = await axiosPrivate.post("/vote", JSON.stringify(vote), {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      });
+      console.log(`Voted ${response.data.vote} for ${response.data.ad.id}`);
+      setVotes([...votes, vote]);
+      setAds(ads.filter((ad) => ad.id !== vote.ad.id));
+      if (ads.length === 1) {
+        setVotes([]);
+        setRunning(false);
       }
-  }
+    } catch (err) {
+      console.error(err);
+      navigate("/login", { state: { from: stateLocation }, replace: true });
+    }
+  };
 
-
-
-
-
-
-
-    // await fetch("http://localhost:3500/save", {
-    //   method: "POST",
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: jsonVote,
-    // })
-      // .then((response) => response.json())
-      // .then((response) => {
-        // console.log(response);
-      // });
-    // If the ad state array is empty, reset vote counter
+  // await fetch("http://localhost:3500/save", {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: jsonVote,
+  // })
+  // .then((response) => response.json())
+  // .then((response) => {
+  // console.log(response);
+  // });
+  // If the ad state array is empty, reset vote counter
   //   if (ads.length === 1) {
   //     setVotes([]);
   //     setRunning(false);
@@ -110,63 +104,64 @@ const AdCard = () => {
 
   return (
     <>
-    <Banner className= "banner-sm"/>
-    <Navbar/>
-    <div className="main_wrapper">
-      <ParamBox type="scraper" text={"Get Ads."} handleClick={scrapeAds} />
+      <Banner className="banner-sm" />
+      <Navbar />
+      <div className="main_wrapper">
+        <ParamBox type="scraper" text={"Get Ads."} handleClick={scrapeAds} />
 
-      {running ? (
-        console.log(ads),
-        // IF ADS ARRAY STATE EMPTY
-        ads.length === 0 ? (
-          <div className="ad">
-            <h2>No more ads!</h2>
-            <h3> Try again later.</h3>
+        {running ? (
+          (console.log(ads),
+          // IF ADS ARRAY STATE EMPTY
+          ads.length === 0 ? (
+            <div className="ad">
+              <h2>No more ads!</h2>
+              <h3> Try again later.</h3>
+              <Footer />
+            </div>
+          ) : (
+            // IF ADS ARRAY STATE NOT EMPTY
+            ads.map((ad, index) => (
+              <div
+                key={index}
+                style={{ display: index === 0 ? "block" : "none" }}
+              >
+                <Ads
+                  id={ad.id}
+                  url={ad.url}
+                  title={ad.title}
+                  alt={ad.desc}
+                  src={ad.img}
+                  price={ad.price}
+                  desc={ad.desc}
+                  index={index}
+                />
+
+                <div className="vote_wrapper">
+                  <VoteButton
+                    ad={ad}
+                    vote={true}
+                    text="Deal"
+                    handleClick={sendVote}
+                  />
+                  <VoteButton
+                    ad={ad}
+                    vote={false}
+                    text="No Deal"
+                    handleClick={sendVote}
+                  />
+                </div>
+              </div>
+            ))
+          ))
+        ) : (
+          // IF NOT RUNNING
+          //TODO Not working as intended
+          <div style={{ cursor: "pointer" }}>
+            <h2>Loading...</h2>
             <Footer />
           </div>
-        ) : (
-          // IF ADS ARRAY STATE NOT EMPTY
-          ads.map((ad, index) => (
-            <div
-              key={index}
-              style={{ display: index === 0 ? "block" : "none" }}
-            >
-              <Ads
-                id={ad.id}
-                url={ad.url}
-                title={ad.title}
-                alt={ad.desc}
-                src={ad.img}
-                price={ad.price}
-                desc={ad.desc}
-                index={index}
-              />
-
-              <div className="vote_wrapper">
-                <VoteButton
-                  ad={ad}
-                  vote={true}
-                  text="Deal"
-                  handleClick={sendVote}
-                />
-                <VoteButton
-                  ad={ad}
-                  vote={false}
-                  text="No Deal"
-                  handleClick={sendVote}
-                />
-              </div>
-            </div>
-          ))
-        )
-      ) : (
-        // IF NOT RUNNING
-        <div style={{ cursor: "pointer" }}>
-          <h2>Loading...</h2>
-          <Footer />
-        </div>
-      )}
-    </div>
+        )}
+      </div>
     </>
   );
 };
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -6,20 +6,37 @@ import Navbar from "./Navbar";
 import Users from "./Users";
 import { useNavigate, Link } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
+import useAuth from "../hooks/useAuth";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
 
 const Settings = () => {
-
-  const logout  = useLogout();;
+  const logout = useLogout();
   const navigate = useNavigate();
+  const { auth } = useAuth();
+  const axiosPrivate = useAxiosPrivate();
 
   const signOut = async () => {
-      await logout();
-      navigate('/login');
-  }
+    await logout();
+    navigate("/login");
+  };
 
   //  TODO: Save to local storage
   const handleClick = async (pref) => {
-    const data = JSON.stringify(pref);
+    try {
+      const response = await axiosPrivate.post(
+        "/vote",
+        JSON.stringify({ pref, user: auth.user }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+    } catch (err) {
+      console.error(err);
+    }
+
+    console.log(auth.user);
+    const data = JSON.stringify({ pref, user: auth.user });
     await fetch("http://localhost:3500/pref", {
       method: "PUT",
       headers: {
@@ -27,22 +44,28 @@ const Settings = () => {
         "Content-Type": "application/json",
       },
       body: data,
-    }).then((response) => response.json());
+    })
+      .then((response) => response.json())
+      .then((response) => console.log(response));
   };
 
   return (
-    <><Banner className = "banner-sm"/><Navbar/>
-    <div className="main_wrapper">
-      <ParamBox
-        type="setting"
-        text="Save preferences."
-        handleClick={handleClick}
-      />
-      <div >
-        <button className = "login-btn" onClick={signOut}>Sign Out</button>
+    <>
+      <Banner className="banner-sm" />
+      <Navbar />
+      <div className="main_wrapper">
+        <ParamBox
+          type="setting"
+          text="Save preferences."
+          handleClick={handleClick}
+        />
+        <div>
+          <button className="login-btn" onClick={signOut}>
+            Sign Out
+          </button>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
     </>
   );
 };
