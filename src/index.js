b41e1c532463ad2a9fd6d8c473b4ee198412c9e4
ellis0n <100211236+ellis0n@users.noreply.github.com>
2023-08-20 21:14:17 -0230

    fiddle

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
+	// <React.StrictMode>
 		<BrowserRouter>
 			<AuthProvider>
 				<Routes>
@@ -16,6 +16,6 @@ root.render(
 				</Routes>
 			</AuthProvider>
 		</BrowserRouter>
-	</React.StrictMode>
+	{/* </React.StrictMode> */}
 );
 reportWebVitals();
