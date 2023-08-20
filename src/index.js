27d4981564ad0280c46b6eeec573f8d61868e8a1
ellis0n <100211236+ellis0n@users.noreply.github.com>
2023-08-20 20:24:05 -0230

    hashrouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,9 +11,11 @@ root.render(
 	// <React.StrictMode>
 	<BrowserRouter>
 		<AuthProvider>
-			<Routes>
-				<Route path="/*" element={<App />} />
-			</Routes>
+			<HashRouter>
+				<Routes>
+					<Route path="/*" element={<App />} />
+				</Routes>
+			</HashRouter>
 		</AuthProvider>
 	</BrowserRouter>
 	//</React.StrictMode>
