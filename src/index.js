1fd388a086b5574524e2ace4b52a2038a4fdb90b
ellis0n <100211236+ellis0n@users.noreply.github.com>
2023-08-20 21:22:49 -0230

    strictmode

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import { AuthProvider } from "./context/AuthProvider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	// <React.StrictMode>
-		<BrowserRouter>
-			<AuthProvider>
-				<Routes>
-					<Route path="/*" element={<App />} />
-				</Routes>
-			</AuthProvider>
-		</BrowserRouter>
-	{/* </React.StrictMode> */}
+	<BrowserRouter>
+		<AuthProvider>
+			<Routes>
+				<Route path="/*" element={<App />} />
+			</Routes>
+		</AuthProvider>
+	</BrowserRouter>
+	//  </React.StrictMode>
 );
 reportWebVitals();
