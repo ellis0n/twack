95a5b1172193049e9a08d46e7af503a1e33c7063
ellis0n <100211236+ellis0n@users.noreply.github.com>
2023-08-20 17:01:31 -0230

    gh-pages

--- src/helper/pref.js
+++ src/helper/pref.js
@@ -1,26 +1,26 @@
-useEffect(() => {
-	let isMounted = true;
-	const controller = new AbortController();
+// useEffect(() => {
+// 	let isMounted = true;
+// 	const controller = new AbortController();
 
-	const getPref = async () => {
-		try {
-			const response = await axiosPrivate.get("/pref", {
-				signal: controller.signal,
-			});
-			isMounted &&
-				setParams({
-					location: response.data.pref.location,
-					category: response.data.pref.category,
-				});
-		} catch (err) {
-			console.error(err);
-			navigate("/login", { state: { from: location }, replace: true });
-		}
-	};
-	getPref();
+// 	const getPref = async () => {
+// 		try {
+// 			const response = await axiosPrivate.get("/pref", {
+// 				signal: controller.signal,
+// 			});
+// 			isMounted &&
+// 				setParams({
+// 					location: response.data.pref.location,
+// 					category: response.data.pref.category,
+// 				});
+// 		} catch (err) {
+// 			console.error(err);
+// 			navigate("/login", { state: { from: location }, replace: true });
+// 		}
+// 	};
+// 	getPref();
 
-	return () => {
-		isMounted = false;
-		controller.abort();
-	};
-}, []);
+// 	return () => {
+// 		isMounted = false;
+// 		controller.abort();
+// 	};
+// }, []);
