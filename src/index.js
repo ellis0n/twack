54f4c479975e6bb41efa26f5be64124caf271903
ellis0n <100211236+ellis0n@users.noreply.github.com>
2023-08-20 20:31:12 -0230

    hashrouter fix

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ import { AuthProvider } from "./context/AuthProvider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	// <React.StrictMode>
-	<BrowserRouter>
+	<HashRouter>
+		{/* <BrowserRouter> */}
 		<AuthProvider>
-			<HashRouter>
-				<Routes>
-					<Route path="/*" element={<App />} />
-				</Routes>
-			</HashRouter>
+			<Routes>
+				<Route path="/*" element={<App />} />
+			</Routes>
 		</AuthProvider>
-	</BrowserRouter>
+		{/* </BrowserRouter> */}
+	</HashRouter>
 	//</React.StrictMode>
 );
 reportWebVitals();
